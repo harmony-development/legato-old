31aaa9447618391d8f1dc3dc3507b8ad050a45d3
Danil Korennykh <bluskript@gmail.com>
2019-10-22 12:53:00 -0400

    Begin working on rewriting server from scratch for scalability
    Node really isn't going to cut it, we'll be using a mix of elixir, golang, and other languages

--- build/routes/login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var routes = express_1.Router();
-routes.get('/login', function (req, res) {
-    res.json({ status: 'online' });
-});
-exports.default = routes;
--- build/routes/register.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var routes = express_1.Router();
-routes.get('/register', function (req, res) {
-    res.json({ status: 'online' });
-});
-exports.default = routes;
