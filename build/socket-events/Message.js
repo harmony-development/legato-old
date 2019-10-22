b316344846a21e9fa13c0e7a43a0456edb02de0e
Danil Korennykh <bluskript@gmail.com>
2019-10-21 20:44:35 -0400

    code cleanup : put JWT verification in a single function

--- build/socket-events/Message.js
+++ build/socket-events/Message.js
@@ -1,43 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var types_1 = require("../types");
-var jwt_1 = require("../promisified/jwt");
 var __1 = require("..");
 var userSchema_1 = require("../schema/userSchema");
 function onMessage(socket) {
     socket.on(types_1.Events.MESSAGE, function (data) {
-        jwt_1.verify(data.token, __1.config.config.jwtsecret)
-            .then(function (result) {
-            if (result.valid && result.decoded) {
-                if (result.decoded.userid) {
-                    userSchema_1.User.findOne({ userid: result.decoded.userid })
-                        .then(function (user) {
-                        if (user) {
-                            __1.harmonyServer.getSocketServer().emit(types_1.Events.MESSAGE, {
-                                username: user.username,
-                                message: data.message,
-                                avatar: user.avatar,
-                                files: data.files
-                            });
-                        }
-                    })
-                        .catch(function (err) {
-                        console.log(err);
+        __1.harmonyServer.Database.verifyToken(data.token)
+            .then(function (userid) {
+            userSchema_1.User.findOne({ userid: userid })
+                .then(function (user) {
+                if (user) {
+                    __1.harmonyServer.getSocketServer().emit(types_1.Events.MESSAGE, {
+                        username: user.username,
+                        message: data.message,
+                        avatar: user.avatar,
+                        files: data.files
                     });
                 }
-                else {
-                    console.log('Missing userid');
-                    socket.emit(types_1.Events.INVALIDATE_SESSION, 'invalid session token');
-                }
-            }
-            else {
-                console.log('invalid jwt');
-                socket.emit(types_1.Events.INVALIDATE_SESSION, 'invalid session token');
-            }
+            })
+                .catch(function (err) {
+                console.log(err);
+            });
         })
-            .catch(function (err) {
-            console.log(err);
-            socket.emit(types_1.Events.INVALIDATE_SESSION, 'invalid session token');
+            .then(function () {
+            __1.harmonyServer
+                .getSocketServer()
+                .emit(types_1.Events.INVALIDATE_SESSION, 'Invalid token');
         });
     });
 }
--- build/socket-events/UpdateUser.js
+++ build/socket-events/UpdateUser.js
@@ -1,45 +1,38 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var types_1 = require("../types");
-var jwt_1 = require("../promisified/jwt");
 var __1 = require("..");
 var userSchema_1 = require("../schema/userSchema");
 function onUpdateUser(socket) {
     socket.on(types_1.Events.PROFILE_UPDATE, function (data) {
-        if (typeof data.token === 'string') {
-            jwt_1.verify(data.token, __1.config.config.jwtsecret)
-                .then(function (result) {
-                if (result.valid) {
-                    if (result.decoded.userid) {
-                        userSchema_1.User.findOne({ userid: result.decoded.userid })
-                            .then(function (user) {
-                            if (user) {
-                                if (data.avatar) {
-                                    user.avatar = data.avatar;
-                                }
-                                if (data.username) {
-                                    user.username = data.username;
-                                }
-                                if (data.theme) {
-                                    user.theme = data.theme;
-                                }
-                                user.save();
-                            }
-                            else {
-                                socket.emit(types_1.Events.PROFILE_UPDATE_ERROR, 'You do not exist');
-                            }
-                        })
-                            .catch(function (err) {
-                            console.log(err);
-                            socket.emit(types_1.Events.PROFILE_UPDATE_ERROR, 'Failed to update profile');
-                        });
+        __1.harmonyServer.Database.verifyToken(data.token)
+            .then(function (userid) {
+            userSchema_1.User.findOne({ userid: userid })
+                .then(function (user) {
+                if (user) {
+                    if (data.avatar) {
+                        user.avatar = data.avatar;
                     }
+                    if (data.username) {
+                        user.username = data.username;
+                    }
+                    if (data.theme) {
+                        user.theme = data.theme;
+                    }
+                    user.save();
+                }
+                else {
+                    socket.emit(types_1.Events.PROFILE_UPDATE_ERROR, 'You do not exist');
                 }
             })
-                .catch(function () {
-                socket.emit(types_1.Events.PROFILE_UPDATE_ERROR, 'Missing Token');
+                .catch(function (err) {
+                console.log(err);
+                socket.emit(types_1.Events.PROFILE_UPDATE_ERROR, 'Failed to update profile');
             });
-        }
+        })
+            .catch(function () {
+            __1.harmonyServer.SocketServer.emit(types_1.Events.INVALIDATE_SESSION, 'Invalid token');
+        });
     });
 }
 exports.default = onUpdateUser;
