e77966e727f144a9fa492f165512659b7ef1c248
Danil Korennykh <bluskript@gmail.com>
2019-10-23 21:29:35 -0400

    switch to dep instead

--- server.go
+++ server.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"strconv"
 
-	"./rest"
+	"github.com/bluskript/harmony-server/rest"
 	"github.com/julienschmidt/httprouter"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func startMongoServer() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func startServer(port int, callback func(error)) {
 	router := httprouter.New()
 	http.Handle("/", http.FileServer(http.Dir("public/")))
 	router.GET("/ping", rest.Ping)
+
+	go startMongoServer()
+
 	log.Println("Server successfully started on port " + strconv.Itoa(port))
 	callback(http.ListenAndServe(":"+strconv.Itoa(port), router))
 }
