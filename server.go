f151bec4c409c3c23468bf1d500d6b805ad6f9f3
Danil Korennykh <bluskript@gmail.com>
2019-11-15 12:39:07 -0500

    add an endpoint to upload files

--- server.go
+++ server.go
@@ -22,12 +22,11 @@ func openDatabase() {
 	if err != nil {
 		log.Fatal(Red("unable to open harmony.db, harmony cannot continue " + err.Error()).Bold())
 	}
-	statement, err := database.Prepare(`CREATE TABLE IF NOT EXISTS users (
+		statement, err := database.Prepare(`CREATE TABLE IF NOT EXISTS users (
 		id TEXT NOT NULL PRIMARY KEY UNIQUE,
 		email TEXT UNIQUE NOT NULL,
 		password TEXT NOT NULL,
-		username TEXT
-	)`)
+		username TEXT)`)
 	if err != nil {
 		log.Fatal(Red("Cannot prepare database initialization statements, harmony cannot continue. " + err.Error()).Bold())
 	}
@@ -58,10 +57,18 @@ func startServer(port int, callback func(error)) {
 		log.Fatal(Red("Unable to read .env file! Please assign JWT_SECRET variable in .env!").Bold())
 	}
 
+	if _, err := os.Stat("./storage/"); os.IsNotExist(err) {
+		err = os.Mkdir("./storage/", 0700)
+
+		if err != nil {
+			log.Fatal(Red("Error making storage folder").Bold(), err)
+		}
+	}
+
 	globals.HarmonyServer.JwtSecret = os.Getenv("JWT_SECRET")
 	globals.HarmonyServer.Router = mux.NewRouter()
-	globals.HarmonyServer.Router.Handle("/", http.FileServer(http.Dir("public/")))
-	globals.HarmonyServer.Router.HandleFunc("/api/ping", rest.Ping)
+	globals.HarmonyServer.Router.PathPrefix("/storage/").Handler(http.StripPrefix("/storage/", http.FileServer(http.Dir("storage"))))
+	globals.HarmonyServer.Router.HandleFunc("/api/attach", rest.HandleAttachment)
 	globals.HarmonyServer.Router.HandleFunc("/api/socket", websocketHandler)
 	openDatabase()
 	log.Println(Green("Server successfully started on port " + strconv.Itoa(port)).Bold())
