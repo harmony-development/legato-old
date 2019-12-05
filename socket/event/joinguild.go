2af119399c2811a5ba71a5d7781820e409289600
Danil Korennykh <bluskript@gmail.com>
2019-12-05 02:52:35 -0500

    add guild creation capabilities

--- socket/event/joinguild.go
+++ socket/event/joinguild.go
@@ -24,12 +24,24 @@ func OnJoinGuild(ws *socket.Client, rawMap map[string]interface{}) {
 		return
 	}
 	if data.InviteCode, ok = rawMap["invitecode"].(string); !ok {
+		ws.Send(&socket.Packet{
+			Type: "joinguild",
+			Data: map[string]interface{}{
+				"message": "Invalid Invite Code!",
+			},
+		})
 		return
 	}
 	var guildid string
 	err := harmonydb.DBInst.QueryRow("SELECT guildid FROM invites WHERE inviteid=?", data.InviteCode).Scan(&guildid)
 	if err != nil {
-		golog.Warnf("Error getting invite guild. This probably means something is very screwed up with the guildid. %v", err)
+		ws.Send(&socket.Packet{
+			Type: "joinguild",
+			Data: map[string]interface{}{
+				"message": "Invalid Invite Code!",
+			},
+		})
+		golog.Warnf("Error getting invite guild. This probably means the guild invite code doesn't exist. %v", err)
 		return
 	}
 	_, err = harmonydb.DBInst.Exec("INSERT INTO guildmembers(userid, guildid) VALUES(?, ?)", userid, guildid)
