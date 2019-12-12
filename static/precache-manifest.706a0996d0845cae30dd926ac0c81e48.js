047163d4f145ef6efe48a432a73ae9c823209f6c
Danil Korennykh <bluskript@gmail.com>
2019-12-11 19:05:06 -0500

    add production route handling

---
+++ static/precache-manifest.706a0996d0845cae30dd926ac0c81e48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5743452a81e96b757f76691c36db2cb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62ea94ee0630410a109e",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "45777a4692f465eecba5",
+    "url": "/static/css/main.73de8e3f.chunk.css"
+  },
+  {
+    "revision": "62ea94ee0630410a109e",
+    "url": "/static/js/2.15bfeedf.chunk.js"
+  },
+  {
+    "revision": "c0060d946240deb53d26fda52cafc461",
+    "url": "/static/js/2.15bfeedf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45777a4692f465eecba5",
+    "url": "/static/js/main.d89c5ff9.chunk.js"
+  },
+  {
+    "revision": "4c1c317f0fde0c70ba60",
+    "url": "/static/js/runtime-main.4bd86ca1.js"
+  }
+]);
