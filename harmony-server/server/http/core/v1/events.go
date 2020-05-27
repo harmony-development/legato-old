69e6c2b4b86b94c8e7536c8703b28867035a099c
Carson Black <uhhadd@gmail.com>
2020-05-26 20:44:12 -0400

    snake_case_the_json

--- harmony-server/server/http/core/v1/events.go
+++ harmony-server/server/http/core/v1/events.go
@@ -2,19 +2,19 @@ package v1
 
 const (
 	ActionEventType        = "action"
-	ChannelCreateEventType = "channelCreate"
-	ChannelDeleteEventType = "channelDelete"
-	AvatarUpdateEventType  = "avatarUpdate"
-	GuildDeleteEventType   = "guildDelete"
-	MessageDeleteEventType = "messageDelete"
+	ChannelCreateEventType = "channel_create"
+	ChannelDeleteEventType = "channel_delete"
+	AvatarUpdateEventType  = "avatar_update"
+	GuildDeleteEventType   = "guild_delete"
+	MessageDeleteEventType = "message_delete"
 )
 
 // ActionEvent is the data that will be sent to a client on an action trigger
 type ActionEvent struct {
-	GuildID   uint64 `json:"guildID"`
-	ChannelID uint64 `json:"channelID"`
-	MessageID uint64 `json:"messageID"`
-	TriggerID uint64 `json:"triggerID"`
+	GuildID   uint64 `json:"guild_id"`
+	ChannelID uint64 `json:"channel_id"`
+	MessageID uint64 `json:"message_id"`
+	TriggerID uint64 `json:"trigger_id"`
 	Action    struct {
 		ID   string `json:"id"`
 		Data string `json:"data"`
@@ -23,31 +23,31 @@ type ActionEvent struct {
 
 // ChannelCreateEvent is the data that will be sent to a client on a channel create
 type ChannelCreateEvent struct {
-	GuildID     uint64 `json:"guildID"`
-	ChannelName string `json:"channelName"`
-	ChannelID   uint64 `json:"channelID"`
+	GuildID     uint64 `json:"guild_id"`
+	ChannelName string `json:"channel_name"`
+	ChannelID   uint64 `json:"channel_id"`
 }
 
 // ChannelDeleteEvent is the data that will be sent to a client on a channel delete
 type ChannelDeleteEvent struct {
-	ChannelID uint64 `json:"channelID"`
-	GuildID   uint64 `json:"guildID"`
+	ChannelID uint64 `json:"channel_id"`
+	GuildID   uint64 `json:"guild_id"`
 }
 
 // AvatarUpdateEvent is the data that will be sent to a client on an avatar update
 type AvatarUpdateEvent struct {
-	UserID    uint64 `json:"userID"`
-	NewAvatar string `json:"newAvatar"`
+	UserID    uint64 `json:"user_id"`
+	NewAvatar string `json:"new_avatar"`
 }
 
 // GuildDeleteEvent is the data that will be sent to a client on a guild delete
 type GuildDeleteEvent struct {
-	GuildID uint64 `json:"guildID"`
+	GuildID uint64 `json:"guild_id"`
 }
 
 // MessageDeleteEvent is the data that will be sent to a client on a message delete
 type MessageDeleteEvent struct {
-	GuildID   uint64 `json:"guildID"`
-	ChannelID uint64 `json:"channelID"`
-	MessageID uint64 `json:"messageID"`
+	GuildID   uint64 `json:"guild_id"`
+	ChannelID uint64 `json:"channel_id"`
+	MessageID uint64 `json:"message_id"`
 }
