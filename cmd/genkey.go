1035dd16a8360ae4efca3af7a9ac81bd9ad34988
Danil Korennykh <bluskript@gmail.com>
2020-06-19 16:14:40 -0400

    fix key generation

--- cmd/genkey.go
+++ cmd/genkey.go
@@ -4,7 +4,6 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
-	"encoding/asn1"
 	"encoding/pem"
 	"os"
 
@@ -17,27 +16,29 @@ func GenKeys() {
 		logrus.Fatal(err)
 	}
 	pubKey := key.PublicKey
-	pubKeyBytes, err := asn1.Marshal(pubKey)
+	pubKeyBytes, err := x509.MarshalPKIXPublicKey(&pubKey)
 	if err != nil {
 		logrus.Fatal(err)
 	}
-	var keyPem = &pem.Block{
-		Type:  "PRIVATE KEY",
+	var privKeyPem = &pem.Block{
+		Type:  "RSA PRIVATE KEY",
 		Bytes: x509.MarshalPKCS1PrivateKey(key),
 	}
 	var pubKeyPem = &pem.Block{
-		Type:  "PUBLIC KEY",
+		Type:  "RSA PUBLIC KEY",
 		Bytes: pubKeyBytes,
 	}
 	privKeyOut, err := os.Create("harmony-key.pem")
 	if err != nil {
 		logrus.Fatal(err)
 	}
+	defer privKeyOut.Close()
 	pubKeyOut, err := os.Create("harmony-key.pub")
 	if err != nil {
 		logrus.Fatal(err)
 	}
-	if err := pem.Encode(privKeyOut, keyPem); err != nil {
+	defer pubKeyOut.Close()
+	if err := pem.Encode(privKeyOut, privKeyPem); err != nil {
 		logrus.Fatal(err)
 	}
 	if err := pem.Encode(pubKeyOut, pubKeyPem); err != nil {
