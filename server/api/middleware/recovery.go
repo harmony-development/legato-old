23b865e709edeea3164eaf91c2760ded51947833
Carson Black <uhhadd@gmail.com>
2020-07-19 00:31:59 -0400

    Log stack trace

--- server/api/middleware/recovery.go
+++ server/api/middleware/recovery.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"errors"
+	"runtime/debug"
+
 	"github.com/harmony-development/legato/server/http/responses"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -10,5 +13,6 @@ func (m Middlewares) RecoveryFunc(p interface{}) error {
 	if err, ok := p.(error); ok {
 		m.Logger.Exception(err)
 	}
+	m.Logger.Exception(errors.New(string(debug.Stack())))
 	return status.Error(codes.Internal, responses.UnknownError)
 }
