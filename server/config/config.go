f9a25e5960bb411445010023fc4ee5d369fd133c
Jan Blackquill <uhhadd@gmail.com>
2020-12-25 20:27:57 -0500

    Unscuff config; unfortunately requires excess blocks again

--- server/config/config.go
+++ server/config/config.go
@@ -16,7 +16,7 @@ import (
 )
 
 type Config struct {
-	Server *struct {
+	Server struct {
 		Host           string `hcl:"Host,optional" default:"0.0.0.0"`
 		Port           int    `hcl:"Port,optional" default:"2289"`
 		PrivateKeyPath string `hcl:"PrivateKeyPath,optional" default:"harmony-key.pem"`
@@ -25,18 +25,18 @@ type Config struct {
 		SnowflakeStart int64  `hcl:"SnowflakeStart,optional" default:"0"`
 		UseCORS        bool   `hcl:"UseCORS,optional" default:"true"`
 
-		Policies *struct {
-			Avatar *struct {
+		Policies struct {
+			Avatar struct {
 				Width   int  `hcl:"Width,optional" default:"256"`
 				Height  int  `hcl:"Height,optional" default:"256"`
 				Quality int  `hcl:"Quality,optional" default:"50"`
 				Crop    bool `hcl:"Crop,optional" default:"true"`
 			} `hcl:"Avatar,block"`
-			Username *struct {
+			Username struct {
 				MinLength int `hcl:"MinLength,optional" default:"2"`
 				MaxLength int `hcl:"MaxLength,optional" default:"32"`
 			} `hcl:"Username,block"`
-			Password *struct {
+			Password struct {
 				MinLength  int `hcl:"MinLength,optional" default:"5"`
 				MaxLength  int `hcl:"MaxLength,optional" default:"256"`
 				MinLower   int `hcl:"MinLower,optional" default:"1"`
@@ -44,35 +44,36 @@ type Config struct {
 				MinNumbers int `hcl:"MinNumbers,optional" default:"1"`
 				MinSymbols int `hcl:"MinSymbols,optional" default:"0"`
 			} `hcl:"Password,block"`
-			Attachments *struct {
+			Attachments struct {
 				MaximumCount int `hcl:"MaximumCount,optional" default:"10"`
 			} `hcl:"Attachments,block"`
-			Debug *struct {
+			Debug struct {
 				LogErrors                  bool `hcl:"LogErrors,optional" default:"true"`
 				LogRequests                bool `hcl:"LogRequests,optional" default:"true"`
 				RespondWithErrors          bool `hcl:"RespondWithErrors,optional" default:"false"`
 				ResponseErrorsIncludeTrace bool `hcl:"ResponseErrorsIncludeTrace" default:"true"`
 				VerboseStreamHandling      bool `hcl:"VerboseStreamHandling" default:"false"`
 			} `hcl:"Debug,block"`
-			Sessions *struct {
+			Sessions struct {
 				Duration time.Duration `hcl:"Duration,optional" default:"172800000000000"`
 			} `hcl:"Sessions,block"`
-			MaximumCacheSizes *struct {
-				Owner    int `hcl:"Owner,optional" default:"5096"`
-				Sessions int `hcl:"Sessions,optional" default:"5096"`
+			MaximumCacheSizes struct {
+				Owner      int `hcl:"Owner,optional" default:"5096"`
+				Sessions   int `hcl:"Sessions,optional" default:"5096"`
+				LinkEmbeds int `hcl:"LinkEmbeds,optional" default:"65536"`
 			} `hcl:"Caches,block"`
-			APIs *struct {
-				Messages *struct {
+			APIs struct {
+				Messages struct {
 					MaximumGetAmount int `hcl:"MaximumGetAmount,optional" default:"50"`
 				} `hcl:"Messages,block"`
 			} `hcl:"APIs,block"`
-			Federation *struct {
+			Federation struct {
 				NonceLength                       int `hcl:"NonceLength,optional" default:"32"`
 				GuildLeaveNotificationQueueLength int `hcl:"GuildLeaveNotificationQueueLength,optional" default:"64"`
 			} `hcl:"Federation,block"`
 		} `hcl:"Policies,block"`
 	} `hcl:"Server,block"`
-	Database *struct {
+	Database struct {
 		Host     string `hcl:"Host,optional" default:"127.0.0.1"`
 		Username string `hcl:"Username"`
 		Password string `hcl:"Password"`
@@ -80,10 +81,10 @@ type Config struct {
 		SSL      bool   `hcl:"SSL,optional" default:"false"`
 		Name     string `hcl:"Name,optional" default:"harmony"`
 	} `hcl:"Database,block"`
-	Flatfile *struct {
+	Flatfile struct {
 		MediaPath string `hcl:"MediaPath,optional" default:"flatfile"`
 	} `hcl:"Flatfile,block"`
-	Sentry *struct {
+	Sentry struct {
 		DSN               string `hcl:"DSN,optional"`
 		AttachStacktraces bool   `hcl:"AttachStacktraces,optional" default:"true"`
 		Enabled           bool   `hcl:"Enabled,optional" default:"false"`
