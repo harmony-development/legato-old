9058b86041d46e8233061464d85bb2954422dca5
Danil Korennykh <bluskript@gmail.com>
2020-12-29 15:18:19 -0500

    debug stack on recovery

--- server/api/middleware/recovery.go
+++ server/api/middleware/recovery.go
@@ -10,10 +10,6 @@ import (
 )
 
 func (m Middlewares) RecoveryFunc(p interface{}) error {
-	if err, ok := p.(error); ok {
-		m.Logger.Exception(err)
-	} else {
-		m.Logger.Exception(errors.New(string(debug.Stack())))
-	}
+	m.Logger.Exception(errors.New(string(debug.Stack())))
 	return status.Error(codes.Internal, responses.UnknownError)
 }
