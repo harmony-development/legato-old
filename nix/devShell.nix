599a0fd86c25b45ad5f52d82837ced5d92a0f957
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-01-21 04:17:55 +0300

    build(nix): change to new cachix cache, also add to CI

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -7,8 +7,8 @@ devshell.mkShell {
     PROTOC = "${protobuf}/bin/protoc";
     PROTOC_INCLUDE = "${protobuf}/include";
     NIX_CONFIG = ''
-      substituters = https://cache.nixos.org https://legato.cachix.org
-      trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= legato.cachix.org-1:nz70bLTILsKu/HLxiEbk33jjfYxzeFgSvKzfwejwBTQ=
+      substituters = https://cache.nixos.org https://harmony.cachix.org
+      trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= harmony.cachix.org-1:yv78QZHgS0UHkrMW56rccNghWHRz18fFRl8mWQ63M6E=
     '';
   };
 }
