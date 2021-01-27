604b33f02225f7d7af2a5eeaae4b9cbe5afe9a49
Danil Korennykh <bluskript@gmail.com>
2021-01-26 20:59:50 -0500

    unfinished hrpc shenanigans

--- genprotos.sh
+++ genprotos.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 mkdir -p "gen"
+
+go build -o ./hrpc/hrpc-bin ./hrpc/main.go
+
 for dir in $(find "protocol" -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq); do
-  echo $(find "${dir}" -name '*.proto')
-  protoc --experimental_allow_proto3_optional \
-  --proto_path=protocol \
-  --proto_path=$(go env GOPATH)/src/github.com/envoyproxy/protoc-gen-validate \
-  --go_out=./gen \
-  --go_opt="plugins=grpc" \
-  --validate_out="lang=go:gen" \
-  $(find "${dir}" -name '*.proto')
+    echo "Generating files in ${dir}..."
+    find "${dir}" -name '*.proto'
+
+    protoc --experimental_allow_proto3_optional \
+    --proto_path=protocol \
+    --plugin=protoc-gen-custom=./hrpc/hrpc-bin \
+    --custom_out=./gen \
+    --custom_opt="/templates/hrpc-server-go.htmpl" \
+    --go_out=./gen \
+    --validate_out="lang=go:gen" \
+    $(find "${dir}" -name '*.proto')
 done
 
-rsync -a -v gen/github.com/harmony-development/legato/gen/ ./gen
+rsync -a -v --remove-source-files gen/github.com/harmony-development/legato/gen/ ./gen
+
+go fmt ./gen/./...
