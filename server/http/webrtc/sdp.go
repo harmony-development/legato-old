7f371f502b7f42c4b64c2b6b4edba6d441203493
Jan Blackquill <uhhadd@gmail.com>
2021-02-03 21:41:59 -0500

    fix: comment dysfunctional code

--- server/http/webrtc/sdp.go
+++ server/http/webrtc/sdp.go
@@ -1,127 +1,127 @@
 package webrtc
 
-import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"strconv"
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"io"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"strconv"
 
-	"github.com/harmony-development/legato/server/http/hm"
-	"github.com/labstack/echo/v4"
-	"github.com/pion/webrtc/v3"
-)
+// 	"github.com/harmony-development/legato/server/http/hm"
+// 	"github.com/labstack/echo/v4"
+// 	"github.com/pion/webrtc/v3"
+// )
 
-// SDPHandler handles webrtc peer connection attempts
-func (api API) SDPHandler(c echo.Context) error {
-	ctx := c.(hm.HarmonyContext)
-	channelID := *ctx.Location.ChannelID
-	body, err := ioutil.ReadAll(ctx.Request().Body)
-	if err != nil {
-		return ctx.NoContent(http.StatusUnprocessableEntity)
-	}
+// // SDPHandler handles webrtc peer connection attempts
+// func (api API) SDPHandler(c echo.Context) error {
+// 	ctx := c.(hm.HarmonyContext)
+// 	channelID := *ctx.Location.ChannelID
+// 	body, err := ioutil.ReadAll(ctx.Request().Body)
+// 	if err != nil {
+// 		return ctx.NoContent(http.StatusUnprocessableEntity)
+// 	}
 
-	if api.VoiceChannels[channelID] == nil {
-		api.VoiceChannels[channelID] = &VoiceChannel{
-			Tracks: make(map[uint64]*webrtc.Track),
-			Peers:  make(map[uint64]*webrtc.PeerConnection),
-		}
-	}
+// 	if api.VoiceChannels[channelID] == nil {
+// 		api.VoiceChannels[channelID] = &VoiceChannel{
+// 			Tracks: make(map[uint64]*webrtc.Track),
+// 			Peers:  make(map[uint64]*webrtc.PeerConnection),
+// 		}
+// 	}
 
-	offer := webrtc.SessionDescription{}
-	if err := json.Unmarshal(body, &offer); err != nil {
-		fmt.Println("error parsing SDP", err)
-		return ctx.NoContent(http.StatusBadRequest)
-	}
+// 	offer := webrtc.SessionDescription{}
+// 	if err := json.Unmarshal(body, &offer); err != nil {
+// 		fmt.Println("error parsing SDP", err)
+// 		return ctx.NoContent(http.StatusBadRequest)
+// 	}
 
-	peerConnection, err := api.MediaAPI.NewPeerConnection(api.peerConnectionConfig)
-	if err != nil {
-		fmt.Println("error making peer connection", err)
-		return ctx.NoContent(http.StatusInternalServerError)
-	}
-	if _, err := peerConnection.AddTransceiverFromKind(webrtc.RTPCodecTypeAudio, webrtc.RtpTransceiverInit{
-		Direction: webrtc.RTPTransceiverDirectionRecvonly,
-	}); err != nil {
-		fmt.Println("error adding transceiver", err)
-		return ctx.NoContent(http.StatusInternalServerError)
-	}
-	peerConnection.OnTrack(api.OnTrackStart(peerConnection, *ctx.Location.ChannelID, ctx.UserID))
-	peerConnection.OnICEConnectionStateChange(api.OnICEConnectionStateChange(peerConnection, *ctx.Location.ChannelID, ctx.UserID))
+// 	peerConnection, err := api.MediaAPI.NewPeerConnection(api.peerConnectionConfig)
+// 	if err != nil {
+// 		fmt.Println("error making peer connection", err)
+// 		return ctx.NoContent(http.StatusInternalServerError)
+// 	}
+// 	if _, err := peerConnection.AddTransceiverFromKind(webrtc.RTPCodecTypeAudio, webrtc.RtpTransceiverInit{
+// 		Direction: webrtc.RTPTransceiverDirectionRecvonly,
+// 	}); err != nil {
+// 		fmt.Println("error adding transceiver", err)
+// 		return ctx.NoContent(http.StatusInternalServerError)
+// 	}
+// 	peerConnection.OnTrack(api.OnTrackStart(peerConnection, *ctx.Location.ChannelID, ctx.UserID))
+// 	peerConnection.OnICEConnectionStateChange(api.OnICEConnectionStateChange(peerConnection, *ctx.Location.ChannelID, ctx.UserID))
 
-	if err := peerConnection.SetRemoteDescription(offer); err != nil {
-		fmt.Println("error setting remote description", err)
-		return ctx.NoContent(http.StatusInternalServerError)
-	}
+// 	if err := peerConnection.SetRemoteDescription(offer); err != nil {
+// 		fmt.Println("error setting remote description", err)
+// 		return ctx.NoContent(http.StatusInternalServerError)
+// 	}
 
-	answer, err := peerConnection.CreateAnswer(nil)
-	if err != nil {
-		fmt.Println("error making answer", err)
-		return ctx.NoContent(http.StatusInternalServerError)
-	}
+// 	answer, err := peerConnection.CreateAnswer(nil)
+// 	if err != nil {
+// 		fmt.Println("error making answer", err)
+// 		return ctx.NoContent(http.StatusInternalServerError)
+// 	}
 
-	if err := peerConnection.SetLocalDescription(answer); err != nil {
-		fmt.Println("error setting local description", err)
-		return ctx.NoContent(http.StatusInternalServerError)
-	}
+// 	if err := peerConnection.SetLocalDescription(answer); err != nil {
+// 		fmt.Println("error setting local description", err)
+// 		return ctx.NoContent(http.StatusInternalServerError)
+// 	}
 
-	for userID := range api.VoiceChannels[*ctx.Location.ChannelID].Tracks {
-		if _, err := peerConnection.AddTrack(api.VoiceChannels[*ctx.Location.ChannelID].Tracks[userID]); err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println("add track!")
-	}
+// 	for userID := range api.VoiceChannels[*ctx.Location.ChannelID].Tracks {
+// 		if _, err := peerConnection.AddTrack(api.VoiceChannels[*ctx.Location.ChannelID].Tracks[userID]); err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		fmt.Println("add track!")
+// 	}
 
-	api.VoiceChannels[*ctx.Location.ChannelID].Peers[ctx.UserID] = peerConnection
+// 	api.VoiceChannels[*ctx.Location.ChannelID].Peers[ctx.UserID] = peerConnection
 
-	return ctx.JSON(http.StatusOK, peerConnection.LocalDescription())
-}
+// 	return ctx.JSON(http.StatusOK, peerConnection.LocalDescription())
+// }
 
-// OnTrackStart handles when a track is being received from a peer
-func (api API) OnTrackStart(peerConnection *webrtc.PeerConnection, channelID, userID uint64) func(remoteTrack *webrtc.Track, receiver *webrtc.RTPReceiver) {
-	return func(remoteTrack *webrtc.Track, receiver *webrtc.RTPReceiver) {
-		localTrack, err := peerConnection.NewTrack(remoteTrack.PayloadType(), remoteTrack.SSRC(), strconv.FormatUint(userID, 10), strconv.FormatUint(userID, 10))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		api.VoiceChannels[channelID].Tracks[userID] = localTrack
-		for userID := range api.VoiceChannels[channelID].Peers {
-			if _, err := api.VoiceChannels[channelID].Peers[userID].AddTrack(localTrack); err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("add track! (after track start)")
-		}
-		rtpBuf := make([]byte, 1460)
-		for {
-			i, readErr := remoteTrack.Read(rtpBuf)
-			if readErr != nil {
-				fmt.Println(readErr)
-				return
-			}
-			if _, err = localTrack.Write(rtpBuf[:i]); err != nil && err != io.ErrClosedPipe {
-				fmt.Println(err)
-				return
-			}
-		}
-	}
-}
+// // OnTrackStart handles when a track is being received from a peer
+// func (api API) OnTrackStart(peerConnection *webrtc.PeerConnection, channelID, userID uint64) func(remoteTrack *webrtc.Track, receiver *webrtc.RTPReceiver) {
+// 	return func(remoteTrack *webrtc.Track, receiver *webrtc.RTPReceiver) {
+// 		localTrack, err := peerConnection.NewTrack(remoteTrack.PayloadType(), remoteTrack.SSRC(), strconv.FormatUint(userID, 10), strconv.FormatUint(userID, 10))
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 		api.VoiceChannels[channelID].Tracks[userID] = localTrack
+// 		for userID := range api.VoiceChannels[channelID].Peers {
+// 			if _, err := api.VoiceChannels[channelID].Peers[userID].AddTrack(localTrack); err != nil {
+// 				fmt.Println(err)
+// 			}
+// 			fmt.Println("add track! (after track start)")
+// 		}
+// 		rtpBuf := make([]byte, 1460)
+// 		for {
+// 			i, readErr := remoteTrack.Read(rtpBuf)
+// 			if readErr != nil {
+// 				fmt.Println(readErr)
+// 				return
+// 			}
+// 			if _, err = localTrack.Write(rtpBuf[:i]); err != nil && err != io.ErrClosedPipe {
+// 				fmt.Println(err)
+// 				return
+// 			}
+// 		}
+// 	}
+// }
 
-// OnICEConnectionStateChange handles webrtc state changes such as timeouts
-func (api API) OnICEConnectionStateChange(peerConnection *webrtc.PeerConnection, channelID, userID uint64) func(webrtc.ICEConnectionState) {
-	return func(state webrtc.ICEConnectionState) {
-		if state == webrtc.ICEConnectionStateDisconnected || state == webrtc.ICEConnectionStateClosed {
-			fmt.Println("disconnect", channelID, userID, state)
-			if err := peerConnection.Close(); err != nil {
-				fmt.Println(err)
-			}
-			if api.VoiceChannels[channelID] != nil {
-				delete(api.VoiceChannels[channelID].Tracks, userID)
-				delete(api.VoiceChannels[channelID].Peers, userID)
-				if len(api.VoiceChannels[channelID].Tracks) == 0 {
-					delete(api.VoiceChannels, channelID)
-				}
-			}
-		}
-	}
-}
+// // OnICEConnectionStateChange handles webrtc state changes such as timeouts
+// func (api API) OnICEConnectionStateChange(peerConnection *webrtc.PeerConnection, channelID, userID uint64) func(webrtc.ICEConnectionState) {
+// 	return func(state webrtc.ICEConnectionState) {
+// 		if state == webrtc.ICEConnectionStateDisconnected || state == webrtc.ICEConnectionStateClosed {
+// 			fmt.Println("disconnect", channelID, userID, state)
+// 			if err := peerConnection.Close(); err != nil {
+// 				fmt.Println(err)
+// 			}
+// 			if api.VoiceChannels[channelID] != nil {
+// 				delete(api.VoiceChannels[channelID].Tracks, userID)
+// 				delete(api.VoiceChannels[channelID].Peers, userID)
+// 				if len(api.VoiceChannels[channelID].Tracks) == 0 {
+// 					delete(api.VoiceChannels, channelID)
+// 				}
+// 			}
+// 		}
+// 	}
+// }
--- server/http/webrtc/webrtc.go
+++ server/http/webrtc/webrtc.go
@@ -1,63 +1,63 @@
 package webrtc
 
-import (
-	"time"
+// import (
+// 	"time"
 
-	"github.com/harmony-development/legato/server/http/routing"
-	"github.com/labstack/echo/v4"
-	"github.com/pion/webrtc/v3"
-)
+// 	"github.com/harmony-development/legato/server/http/routing"
+// 	"github.com/labstack/echo/v4"
+// 	"github.com/pion/webrtc/v3"
+// )
 
-type Dependencies struct {
-	APIGroup *echo.Group
-	Router   routing.IRouter
-}
+// type Dependencies struct {
+// 	APIGroup *echo.Group
+// 	Router   routing.IRouter
+// }
 
-type VoiceChannel struct {
-	Tracks map[uint64]*webrtc.Track
-	Peers  map[uint64]*webrtc.PeerConnection
-}
+// type VoiceChannel struct {
+// 	Tracks map[uint64]*webrtc.Track
+// 	Peers  map[uint64]*webrtc.PeerConnection
+// }
 
-type API struct {
-	*echo.Group
-	Dependencies
-	peerConnectionConfig webrtc.Configuration
-	Engine               webrtc.MediaEngine
-	MediaAPI             *webrtc.API
-	VoiceChannels        map[uint64]*VoiceChannel
-}
+// type API struct {
+// 	*echo.Group
+// 	Dependencies
+// 	peerConnectionConfig webrtc.Configuration
+// 	Engine               webrtc.MediaEngine
+// 	MediaAPI             *webrtc.API
+// 	VoiceChannels        map[uint64]*VoiceChannel
+// }
 
-func New(deps Dependencies) *API {
-	api := &API{
-		Group:        deps.APIGroup,
-		Dependencies: deps,
-		peerConnectionConfig: webrtc.Configuration{
-			ICEServers: []webrtc.ICEServer{
-				{
-					URLs: []string{"stun:stun.l.google.com:19302"},
-				},
-			},
-		},
-		VoiceChannels: make(map[uint64]*VoiceChannel),
-		Engine:        webrtc.MediaEngine{},
-	}
+// func New(deps Dependencies) *API {
+// 	api := &API{
+// 		Group:        deps.APIGroup,
+// 		Dependencies: deps,
+// 		peerConnectionConfig: webrtc.Configuration{
+// 			ICEServers: []webrtc.ICEServer{
+// 				{
+// 					URLs: []string{"stun:stun.l.google.com:19302"},
+// 				},
+// 			},
+// 		},
+// 		VoiceChannels: make(map[uint64]*VoiceChannel),
+// 		Engine:        webrtc.MediaEngine{},
+// 	}
 
-	api.Engine.RegisterCodec(webrtc.NewRTPOpusCodec(webrtc.DefaultPayloadTypeOpus, 48000))
+// 	api.Engine.RegisterCodec(webrtc.NewRTPOpusCodec(webrtc.DefaultPayloadTypeOpus, 48000))
 
-	api.MediaAPI = webrtc.NewAPI(webrtc.WithMediaEngine(api.Engine))
+// 	api.MediaAPI = webrtc.NewAPI(webrtc.WithMediaEngine(api.Engine))
 
-	api.Router.BindRoutes(api.Group, []routing.Route{
-		{
-			Path:    "/:guild_id/:channel_id/sdp",
-			Handler: api.SDPHandler,
-			Auth:    true,
-			RateLimit: &routing.RateLimit{
-				Duration: 3 * time.Second,
-				Burst:    6,
-			},
-			Location: routing.LocationGuildAndChannel,
-			Method:   routing.POST,
-		},
-	})
-	return api
-}
+// 	api.Router.BindRoutes(api.Group, []routing.Route{
+// 		{
+// 			Path:    "/:guild_id/:channel_id/sdp",
+// 			Handler: api.SDPHandler,
+// 			Auth:    true,
+// 			RateLimit: &routing.RateLimit{
+// 				Duration: 3 * time.Second,
+// 				Burst:    6,
+// 			},
+// 			Location: routing.LocationGuildAndChannel,
+// 			Method:   routing.POST,
+// 		},
+// 	})
+// 	return api
+// }
