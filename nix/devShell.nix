5c9285ab41f5c265567ab879a4c8ade88c6a685c
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-02-14 22:21:11 +0300

    chore(deps): update flake deps

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -2,13 +2,19 @@
 with pkgs;
 devshell.mkShell {
   packages = [ go protobuf vips ];
-  env = {
-    GOROOT = "${go}/share/go";
-    PROTOC = "${protobuf}/bin/protoc";
-    PROTOC_INCLUDE = "${protobuf}/include";
-    NIX_CONFIG = ''
-      substituters = https://cache.nixos.org https://harmony.cachix.org
-      trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= harmony.cachix.org-1:yv78QZHgS0UHkrMW56rccNghWHRz18fFRl8mWQ63M6E=
-    '';
-  };
+  env = [
+    {
+      name = "GOROOT";
+      value = "${go}/share/go";
+    }
+    { name = "PROTOC"; value = "${protobuf}/bin/protoc"; }
+    { name = "PROTOC_INCLUDE"; value = "${protobuf}/include"; }
+    {
+      name = "NIX_CONFIG";
+      value = ''
+        substituters = https://cache.nixos.org https://harmony.cachix.org
+        trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= harmony.cachix.org-1:yv78QZHgS0UHkrMW56rccNghWHRz18fFRl8mWQ63M6E=
+      '';
+    }
+  ];
 }
