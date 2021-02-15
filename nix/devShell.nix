d55435015886394e699467fe42ccef80a5cca5d7
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-02-15 17:13:04 +0300

    chore(nix): refactor devshell file

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -3,23 +3,21 @@ with pkgs;
 devshell.mkShell {
   packages = [ protobuf vips ];
   commands = [
-    {
-      package = go;
-    }
+    { package = go; }
   ];
-  env = [
-    {
-      name = "GOROOT";
-      value = "${go}/share/go";
-    }
-    { name = "PROTOC"; value = "${protobuf}/bin/protoc"; }
-    { name = "PROTOC_INCLUDE"; value = "${protobuf}/include"; }
-    {
-      name = "NIX_CONFIG";
-      value = ''
-        substituters = https://cache.nixos.org https://harmony.cachix.org
-        trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= harmony.cachix.org-1:yv78QZHgS0UHkrMW56rccNghWHRz18fFRl8mWQ63M6E=
-      '';
-    }
-  ];
+  env =
+    let
+      nv = lib.nameValuePair;
+    in
+    [
+      (nv "GOROOT" "${go}/share/go")
+      (nv "PROTOC" "${protobuf}/bin/protoc")
+      (nv "PROTOC_INCLUDE" "${protobuf}/include")
+      (
+        nv "NIX_CONFIG" ''
+          substituters = https://cache.nixos.org https://harmony.cachix.org
+          trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= harmony.cachix.org-1:yv78QZHgS0UHkrMW56rccNghWHRz18fFRl8mWQ63M6E=
+        ''
+      )
+    ];
 }
