400522f23e6c169834b6006cc35ecb424b4ba5e1
Bluskript <bluskript@gmail.com>
2021-09-02 01:05:23 -0400

    fancy start prompt

--- logger/logger.go
+++ logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"os"
+	"strings"
 
 	"github.com/apex/log"
 	"github.com/apex/log/handlers/cli"
@@ -18,4 +19,14 @@ func New() *Logger {
 			Level:   log.DebugLevel,
 		},
 	}
+}
+
+func Indent(level log.Level, s string, finish string) string {
+	var output strings.Builder
+	for _, line := range strings.Split(s, "\n") {
+		output.WriteRune('\n')
+		output.WriteString(cli.Colors[level].Sprintf("   ║  ") + line)
+	}
+	output.WriteString(cli.Colors[level].Sprintf("\n   ╚  ") + finish)
+	return output.String()
 }
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/apex/log"
@@ -9,6 +10,7 @@ import (
 	"github.com/harmony-development/legato/api"
 	"github.com/harmony-development/legato/build"
 	"github.com/harmony-development/legato/config"
+	"github.com/harmony-development/legato/logger"
 )
 
 type Server struct {
@@ -17,12 +19,12 @@ type Server struct {
 	c *config.Config
 }
 
-var startupMessage = `	
+var startupMessage = `Version %s
    __                  __      
   / /___  ___ _ ___ _ / /_ ___ 
  / // -_)/ _ ` + "`" + `// _ ` + "`" + `// __// _ \
 /_/ \__/ \_, / \_,_/ \__/ \___/
-        /___/ Version %s
+        /___/ Commit %s
 `
 
 func New(l log.Interface, cfg *config.Config) *Server {
@@ -38,9 +40,22 @@ func New(l log.Interface, cfg *config.Config) *Server {
 	return s
 }
 
+func (s *Server) printStartup() {
+	display := logger.Indent(
+		log.InfoLevel,
+		startupMessage,
+		color.HiMagentaString(fmt.Sprintf("Listening on %s:%d", s.c.Address, s.c.Port)),
+	)
+	versionString := color.GreenString(build.Version)
+	gitString := color.GreenString(
+		fmt.Sprintf("%.7s", build.GitCommit),
+	)
+	s.l.Infof(display, versionString, gitString)
+}
+
 func (s *Server) Start() {
-	s.l.Infof(startupMessage, color.GreenString(build.GitCommit))
-	s.l.Infof("Listening on %s:%d", s.c.Address, s.c.Port)
+	s.printStartup()
+	fmt.Print("   >  ")
 	s.l.WithError(
 		s.Listen(s.c.Address + ":" + strconv.Itoa(s.c.Port)),
 	).Fatal("Fatal error occured")
