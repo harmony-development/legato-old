a4c40626c879fd743d33ec79989049d11ec39cd5
Blusk <52386117+Bluskript@users.noreply.github.com>
2021-09-06 19:50:49 -0400

    refactor to support multiple forms (#65)

--- api/authv1/auth.go
+++ api/authv1/auth.go
@@ -6,6 +6,7 @@ package authv1impl
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/harmony-development/legato/api"
 	"github.com/harmony-development/legato/db/ephemeral"
@@ -17,11 +18,14 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+type formHandlerFunc = func(c context.Context, submission *authv1.NextStepRequest_Form, r *authv1.NextStepRequest) (*authv1.AuthStep, error)
+
 type AuthV1 struct {
 	authv1.DefaultAuthService
-	keyManager key.KeyManager
-	eph        ephemeral.Database
-	persist    persist.Database
+	keyManager   key.KeyManager
+	eph          ephemeral.Database
+	persist      persist.Database
+	formHandlers map[string]formHandlerFunc
 }
 
 var steps = toStepMap(
@@ -41,10 +45,19 @@ func toStepMap(steps ...dynamicauth.Step) map[string]dynamicauth.Step {
 }
 
 func New(keyManager key.KeyManager, eph ephemeral.Database, persist persist.Database) *AuthV1 {
+	a := &AuthV1{
+		keyManager: keyManager,
+		eph:        eph,
+		persist:    persist,
+	}
+
 	return &AuthV1{
 		keyManager: keyManager,
 		eph:        eph,
 		persist:    persist,
+		formHandlers: map[string]formHandlerFunc{
+			loginStep.ID(): a.loginFormHandler,
+		},
 	}
 }
 
@@ -66,76 +79,39 @@ func (v1 *AuthV1) BeginAuth(c context.Context, r *authv1.BeginAuthRequest) (*aut
 	}, nil
 }
 
-func (v1 *AuthV1) NextStep(c context.Context, r *authv1.NextStepRequest) (*authv1.NextStepResponse, error) {
-	currentStepID, err := v1.eph.GetCurrentStep(c, r.AuthId)
+// NextStep handles dyhnamic auth steps
+func (v1 *AuthV1) NextStep(ctx context.Context, r *authv1.NextStepRequest) (*authv1.NextStepResponse, error) {
+	// the ID of the step the user is on
+	currentStepID, err := v1.eph.GetCurrentStep(ctx, r.AuthId)
 	if err != nil {
 		return nil, api.NewError(api.ErrorBadAuthID)
 	}
 
-	// the step the user is currently on
-	step := steps[currentStepID]
-	if choiceStep, ok := step.(*dynamicauth.ChoiceStep); ok {
-		res, err := v1.choiceHandler(c, choiceStep, r)
-		return &authv1.NextStepResponse{
-			Step: res,
-		}, err
-	} else if formStep, ok := step.(*dynamicauth.FormStep); ok {
-		res, err := v1.loginFormHandler(c, formStep, r)
-		return &authv1.NextStepResponse{
-			Step: res,
-		}, err
-	}
-
+	res, err := v1.handleStep(ctx, steps[currentStepID], r)
 	return &authv1.NextStepResponse{
-		Step: steps[currentStepID].ToProtoV1(),
-	}, nil
+		Step: res,
+	}, err
 }
 
-// loginFormHandler handles the login form step
-func (v1 *AuthV1) loginFormHandler(c context.Context, formStep *dynamicauth.FormStep, r *authv1.NextStepRequest) (*authv1.AuthStep, error) {
-	f := r.GetForm()
-	if f == nil {
-		return formStep.ToProtoV1(), nil
+func (v1 *AuthV1) handleStep(ctx context.Context, currentStep dynamicauth.Step, r *authv1.NextStepRequest) (*authv1.AuthStep, error) {
+	switch currentStep := currentStep.(type) {
+	case *dynamicauth.ChoiceStep:
+		return v1.choiceHandler(ctx, currentStep, r)
+	case *dynamicauth.FormStep:
+		formSubmission := r.GetForm()
+		if formSubmission == nil {
+			return currentStep.ToProtoV1(), nil
+		}
+		if err := currentStep.ValidateFormV1(formSubmission); err != nil {
+			return nil, api.NewError(api.ErrorBadFormData)
+		}
+		return v1.formHandlers[currentStep.ID()](ctx, formSubmission, r)
+	default:
+		return nil, fmt.Errorf("user is in an invalid step: %v", currentStep)
 	}
-	if err := formStep.ValidateFormV1(f); err != nil {
-		return nil, api.NewError(api.ErrorBadFormData)
-	}
-
-	email := f.Fields[0].GetString_()
-	provided := f.Fields[1].GetBytes()
-
-	user, local, err := v1.persist.Users().GetLocalByEmail(c, email)
-	if err != nil {
-		return nil, api.NewError(api.ErrorBadCredentials)
-	}
-	if err := bcrypt.CompareHashAndPassword(local.Password, provided); err != nil {
-		// intentionally generic error to give less information to the user
-		return nil, api.NewError(api.ErrorBadCredentials)
-	}
-
-	// login succeessful
-
-	sessionID := randstr.Hex(16)
-
-	if err := v1.persist.Sessions().Add(c, sessionID, user.ID); err != nil {
-		return nil, err
-	}
-
-	if err := v1.eph.DeleteAuthID(c, r.AuthId); err != nil {
-		return nil, err
-	}
-
-	return &authv1.AuthStep{
-		Step: &authv1.AuthStep_Session{
-			Session: &authv1.Session{
-				UserId:       user.ID,
-				SessionToken: sessionID,
-			},
-		},
-	}, nil
 }
 
-// choiceHandler contains logic related to any choice usage
+// choiceHandler contains logic related to any choice step
 func (v1 *AuthV1) choiceHandler(ctx context.Context, choiceStep *dynamicauth.ChoiceStep, r *authv1.NextStepRequest) (*authv1.AuthStep, error) {
 	c := r.GetChoice()
 	if c == nil {
@@ -151,4 +127,47 @@ func (v1 *AuthV1) choiceHandler(ctx context.Context, choiceStep *dynamicauth.Cho
 
 	nextStep := steps[c.Choice]
 	return nextStep.ToProtoV1(), nil
+}
+
+// loginFormHandler handles the login form step
+func (v1 *AuthV1) loginFormHandler(c context.Context, submission *authv1.NextStepRequest_Form, r *authv1.NextStepRequest) (*authv1.AuthStep, error) {
+	email := submission.Fields[0].GetString_()
+	provided := submission.Fields[1].GetBytes()
+
+	user, local, err := v1.persist.Users().GetLocalByEmail(c, email)
+	if err != nil {
+		return nil, api.NewError(api.ErrorBadCredentials)
+	}
+	if err := bcrypt.CompareHashAndPassword(local.Password, provided); err != nil {
+		// intentionally generic error to give less information to the user
+		return nil, api.NewError(api.ErrorBadCredentials)
+	}
+
+	// login succeessful
+	session, err := v1.finishAuth(c, r.AuthId, user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &authv1.AuthStep{
+		Step: &authv1.AuthStep_Session{
+			Session: session,
+		},
+	}, nil
+}
+
+func (v1 *AuthV1) finishAuth(c context.Context, authID string, userID uint64) (*authv1.Session, error) {
+	sessionID := randstr.Hex(16)
+
+	if err := v1.persist.Sessions().Add(c, sessionID, userID); err != nil {
+		return nil, err
+	}
+	if err := v1.eph.DeleteAuthID(c, authID); err != nil {
+		return nil, err
+	}
+
+	return &authv1.Session{
+		UserId:       uint64(userID),
+		SessionToken: sessionID,
+	}, nil
 }
